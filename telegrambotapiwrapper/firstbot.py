2de2ce374c9e6d980c9648a1d1409b4edbfa8696
Dzmitry Maliuzhenets <dzmitrymaliuzhenets@gmail.com>
2019-05-23 22:08:35 +0300

    development

--- telegrambotapiwrapper/firstbot.py
+++ telegrambotapiwrapper/firstbot.py
@@ -4,6 +4,7 @@ sys.path.insert(0, "/home/dzmitry/PycharmProjects/telegrambotapiwrapper/")
 
 
 from telegrambotapiwrapper.wrapper import Api
+from telegrambotapiwrapper.api.types import User
 
 TOKEN = "432916128:AAG-rZvzYDzZCUr2psOlBrYeJa0_is7LR9o"
 # TOKEN = "<your_bot_api_token>"
@@ -11,5 +12,5 @@ TOKEN = "432916128:AAG-rZvzYDzZCUr2psOlBrYeJa0_is7LR9o"
 bot_api = Api(token=TOKEN)
 me = bot_api.get_me()
 print("me: {}".format(me))
-print("type of `me`:{}".format(type(me))) # see type of result
+print("isinstance(me, User): {}".format(isinstance(me, User)))  # see type of result
 
