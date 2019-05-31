d27f0c6252efe1c5c109071b85d4a5d989fbbdaf
Dzmitry Maliuzhenets <dzmitrymaliuzhenets@gmail.com>
2019-05-31 20:42:22 +0300

    development

---
+++ telegrambotapiwrapper/webhooks.py
@@ -0,0 +1,16 @@
+from telegrambotapiwrapper.typelib import Update
+from telegrambotapiwrapper.response import handle_response
+from telegrambotapiwrapper.annotation import AnnotationWrapper
+
+
+def webhook_handler(request: str) -> Update:
+    """Process a request from Telegram Bot Api containing Update.
+
+    Args:
+        request (str): string representing a request from Telegra Bot Api
+    Return:
+        (Update): update object
+    Raises:
+        RequestResultIsNotOk: if the answer contains no result
+    """
+    return handle_response(request, AnnotationWrapper("Update"))
